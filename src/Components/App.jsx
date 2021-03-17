3556c32d1e7f446eba70cee056a72ed9cd16d642
Felmon <felmon@Felmons-MacBook-Air.local>
2021-03-16 21:22:09 -0400

    My first commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Heading from "./Heading"
+import About from "./About"
+
+function App(){
+
+
+return  <div>
+        <Heading/>
+        <About/>
+        </div>
+      
+
+
+
+}
+
+
+
+
+
+export default App;
---
+++ src/Components/Heading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Heading(){
+return <div className="nav">
+   <h1>Weather App</h1>
+   </div>
+
+}
+
+export default Heading;
